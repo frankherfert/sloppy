5f05dc67a6f8eadc8a56b630bb64dcb52dc1ca16
frankherfert <frank.herfert@quandoo.com>
2019-05-14 14:07:56 +0200

    changed init file

--- sloppy/__init__.py
+++ sloppy/__init__.py
@@ -3,5 +3,6 @@ from . import catg
 from . import cont
 from . import utils
 from . import learn
+from . import date_time
 
-__all__ = ['missing', 'catg', 'utils', 'cont', 'learn']
+__all__ = ['missing', 'catg', 'utils', 'cont', 'learn', 'date_time']
